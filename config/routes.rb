74c6168d100ca288ddb5b75f7c7807b998d389b2
Chris Bruen <bruen.chris@gmail.com>
2016-07-30 16:51:49 -0400

    add users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'store#index', as: 'store'
 
-  resources :items, only: [:show, :index]
-  resources :categories, only: [:show, :index]
-  resources :users, only: [:new, :create]
-  resources :carts
-  resources :line_items, only: [:create]
-  resources :orders, only: [:show]
-  resources :sessions, only: [:new, :create, :index]
-  
-  
-  get '/auth/facebook/callback' => 'sessions#create'
-  post 'carts/:id/checkout', to: 'carts#checkout', as: 'checkout'
+
 
 end
